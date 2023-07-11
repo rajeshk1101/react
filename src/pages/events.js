9205db702f76f9d2001eb7cdc2fdb6c50b5b6297
Rajesh K <rajeshk1101@gmail.com>
2023-07-11 12:31:30 +0530

    First Commit

---
+++ src/pages/events.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Events = () => {
+return (
+	<div
+	style={{
+		display: 'flex',
+		justifyContent: 'center',
+		alignItems: 'center',
+		height: '100vh'
+	}}
+	>
+	<h1>Welcome to CWG Events</h1>
+	</div>
+);
+};
+
+export default Events;
