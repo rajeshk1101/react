87806f06417322e353e97a11bc7177feb2e53441
Rajesh K <rajeshk1101@gmail.com>
2023-07-11 13:40:08 +0530

    Create abouts.js

---
+++ pages/abouts.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const About = () => {
+return (
+	<div
+	style={{
+		display: 'flex',
+		justifyContent: 'center',
+		alignItems: 'center',
+		height: '100vh'
+	}}
+	>
+	<h1>CWG Portal for Salesforce SSO.</h1>
+	</div>
+);
+};
+
+export default About;
